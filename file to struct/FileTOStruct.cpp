177cfec821ef6b83b385b0a7b38ee78522fcdc04
SpyrosM17 <smantadake71@gmail.com>
2024-05-16 02:02:34 +0300

    Create FileTOStruct.cpp

---
+++ file to struct/FileTOStruct.cpp
@@ -0,0 +1,18 @@
+#include "read_print.h"  //access read_print.h
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+   
+    Read_Data(); //access Read_Data() from read_print.h
+
+    Print_Data(); //access Print_Data() from read_print.h
+
+    return 0;
+}
+
+
