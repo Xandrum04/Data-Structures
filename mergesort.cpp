b1992e8019e1b42c8a97d750d4e00e2b143353e1
SpyrosM17 <smantadake71@gmail.com>
2024-05-25 13:24:40 +0300

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -81,14 +81,14 @@ int main()
     auto end_time = high_resolution_clock::now();
 
     // Calculate the duration of the sorting
-    auto duration = duration_cast<microseconds>(end_time - start_time);
+    auto duration = duration_cast<nanoseconds>(end_time - start_time);
 
     // Print the contents of the Summedcounts array
     cout << "Summed Birth Counts for each region between 2005 and 2022 :" << endl << endl;
     PrintSummedCounts(Summedcounts, MAXSUMS);
     
     // Print the execution time of the merge sort
-    cout << "MergeSort execution time: " << duration.count() << " microseconds" << endl;
+    cout << "MergeSort execution time: " << duration.count() << " nanoseconds" << endl;
 
     return 0;
 }
